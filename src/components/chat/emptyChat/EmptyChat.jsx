5971f5b2664f16b74335d145aa52386e464bbc08
Soumendu1727 <sadhukhansoumendu@gmail.com>
2024-06-19 18:28:44 +0530

    commit 8EmptyChat page

--- src/components/chat/emptyChat/EmptyChat.jsx
+++ src/components/chat/emptyChat/EmptyChat.jsx
@@ -1,14 +1,29 @@
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
 import React from 'react'
+import { emptyChatImage } from '../../../constants/Data'
 
+const Component = styled(Box)`
+    background: #f8f9fa;
+    padding: 30px 0;
+    text-align: center;
+    height: 100%;
+`
+const Container = styled(Box)`
+    padding: 0 200px;
+`
+const Image = styled('img')({
+    width: 400,
+    marginTop: 100,
+    padding: '8rem'
+})
 
 const EmptyChat = () => {
   return (
-    <Box>
-        <Box>
-            
-        </Box>
-    </Box>
+    <Component>
+        <Container>
+            <Image src={emptyChatImage} alt='EmptyImage' />
+        </Container>
+    </Component>
 
   )
 }
