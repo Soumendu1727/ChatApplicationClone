c30f4df8ae43e302da4008986d19be8a96ef9628
Soumendu1727 <sadhukhansoumendu@gmail.com>
2024-07-01 10:40:25 +0530

    Final commit Frontend

--- src/components/chat/menu/Menu.jsx
+++ src/components/chat/menu/Menu.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { Box } from '@mui/material'
 import Header from './Header'
 import Search from './Search'
+import Conversations from './Conversations'
 
 const Menu = () => {
   return (
         <Box>
             <Header />
             <Search />
+            <Conversations />
         </Box>
   )
 }
